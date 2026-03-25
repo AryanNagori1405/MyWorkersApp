2c8912a14e4155ced2eacf1b31167e672384cc5e
Aryan Nagori <aryannikill123@gmail.com>
2026-03-25 10:57:48 +0530

    initial commit - added initial files

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:my_workers_app/my_workers_app.dart';
+
+void main() {
+  runApp(MyWorkersApp());
+}
---
+++ lib/my_workers_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyWorkersApp extends StatelessWidget {
+  const MyWorkersApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
