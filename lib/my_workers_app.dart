f24442ba7039a61b3c626c92f5f99a2c54c94d17
Aryan Nagori <aryannikill123@gmail.com>
2026-03-25 21:54:14 +0530

    completed initial UI of home_screen

--- lib/my_workers_app.dart
+++ lib/my_workers_app.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:my_workers_app/screens/home_screen.dart';
 
 class MyWorkersApp extends StatelessWidget {
   const MyWorkersApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF4361EE),
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
+        textTheme: GoogleFonts.poppinsTextTheme(),
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    );
   }
 }
